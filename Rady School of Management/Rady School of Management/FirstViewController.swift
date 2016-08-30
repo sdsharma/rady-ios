05ae2e9e5c66024aaef86814794936fec2296f6b
Shivam Sharma <sdsharma@ucsd.edu>
2016-08-30 11:59:03 -0700

    Inital Commit
    Starting Tabbed Application

---
+++ Rady School of Management/Rady School of Management/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Rady School of Management
+//
+//  Created by Shivam Sharma on 8/30/16.
+//  Copyright © 2016 Rady School of Management. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
