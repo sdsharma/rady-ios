01122f0309f34a28416a05bd503debef5aead42e
Shivam Sharma <sdsharma@ucsd.edu>
2016-08-30 12:44:00 -0700

    Added initial map

--- Rady School of Management/Rady School of Management/SecondViewController.swift
+++ Rady School of Management/Rady School of Management/SecondViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
